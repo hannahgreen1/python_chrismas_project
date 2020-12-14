eede32a73aa64de7fa4047364744363e2fc71d3e
hannahgreen1 <hannahgreen94@icloud.com>
2020-12-14 16:10:20 +0000

    add templates and controllers

--- controllers/elf_controller.py
+++ controllers/elf_controller.py
@@ -2,3 +2,8 @@ from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.elf import Elf
 from repositories.elf_repository as elf_repository
+
+@elves_blueprint.route("/elves")
+def elves():
+    elves = elf_repository.select_all() # NEW
+    return render_template("elves/index.html", elves = elves)
--- controllers/toy_controller.py
+++ controllers/toy_controller.py
@@ -2,3 +2,9 @@ from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.toy import Toy
 from repositories.toy_repository as toy_repository
+
+@toys_blueprint.route("/toys")
+def toys():
+    toys = toy_repository.select_all() 
+    return render_template("toys/index.html", toys = toys)
+
